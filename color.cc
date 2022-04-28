cd8a233445e7c1f8a8b1907e9d7251e1eb831ad8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-04-28 12:28:02 +0000

    add Color class

---
+++ color.cc
@@ -0,0 +1,31 @@
+/// This file is a part of image_palette_generator
+/// (https://github.com/alexmercerind/image_palette_generator).
+///
+/// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the
+/// LICENSE file.
+
+#include "color.h"
+
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+    : r_(r), g_(g), b_(b), a_(a) {}
+
+Color::Color(int32_t color) {
+  r_ = (color >> 0) & 0xFF;
+  g_ = (color >> 8) & 0xFF;
+  b_ = (color >> 16) & 0xFF;
+  a_ = (color >> 24) & 0xFF;
+}
+
+int32_t Color::ToInt32() {
+  return (r_ << 0) | (g_ << 8) | (b_ << 16) | (a_ << 24);
+}
+
+std::string Color::ToString() {
+  std::stringstream stream;
+  stream << "rgba(" << static_cast<int32_t>(r_) << ", "
+         << static_cast<int32_t>(g_) << ", " << static_cast<int32_t>(b_) << ", "
+         << static_cast<int32_t>(a_) << ")";
+  return stream.str();
+}
---
+++ color.h
@@ -0,0 +1,42 @@
+/// This file is a part of image_palette_generator
+/// (https://github.com/alexmercerind/image_palette_generator).
+///
+/// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the
+/// LICENSE file.
+
+#ifndef COLOR_H_
+#define COLOR_H_
+
+#include <sstream>
+
+class Color {
+ public:
+  uint8_t r() const { return r_; };
+  uint8_t g() const { return g_; };
+  uint8_t b() const { return b_; };
+  uint8_t a() const { return a_; };
+
+  bool operator==(Color color) {
+    return r_ == color.r_ && g_ == color.g_ && b_ == color.b_ && a_ == color.a_;
+  }
+
+  bool operator!=(Color color) { return !(*this == color); }
+
+  Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+  Color(int32_t color);
+
+  int32_t ToInt32();
+
+  std::string ToString();
+
+ private:
+  uint8_t r_;
+  uint8_t g_;
+  uint8_t b_;
+  uint8_t a_;
+};
+
+#endif  // COLOR_H_
