5e64e2d6cbde30abae91919cdbbb19912af9c8b2
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-01 21:08:12 +0530

    feat: ImagePaletteGenerator::GetPalette & ImagePaletteGenerator::GetTones

--- image_palette_generator.cc
+++ image_palette_generator.cc
@@ -21,12 +21,25 @@
 #include "stb_image.h"
 #include "stb_image_resize.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "exoquant.h"
+#ifdef __cplusplus
+}
+#endif
+
+ImagePaletteGenerator::ImagePaletteGenerator() {
+  exoquant_ = exq_init();
+  exq_no_transparency(static_cast<exq_data*>(exoquant_));
+}
+
 void ImagePaletteGenerator::Open(uint8_t* buffer, int32_t size) {
   stbi_image_free(data_);
   data_ = stbi_load_from_memory(buffer, size, &width_, &height_, &channels_, 0);
   pixels_.clear();
   palette_.clear();
-  normalized_palette_.clear();
+  tones_.clear();
   Rescale();
 }
 
@@ -35,16 +48,16 @@ void ImagePaletteGenerator::Open(FILE* file) {
   data_ = stbi_load_from_file(file, &width_, &height_, &channels_, 0);
   pixels_.clear();
   palette_.clear();
-  normalized_palette_.clear();
+  tones_.clear();
   Rescale();
 }
 
-void ImagePaletteGenerator::Open(std::string file_name) {
+void ImagePaletteGenerator::Open(char* file_name) {
   stbi_image_free(data_);
-  data_ = stbi_load(file_name.c_str(), &width_, &height_, &channels_, 0);
+  data_ = stbi_load(file_name, &width_, &height_, &channels_, 0);
   pixels_.clear();
   palette_.clear();
-  normalized_palette_.clear();
+  tones_.clear();
   Rescale();
 }
 
@@ -72,8 +85,8 @@ void ImagePaletteGenerator::Rescale() {
   // Downscale the image dimensions to reduce operations & number of elements in
   // the |pixels_|.
   if (width_ > kRescaleWidth) {
-    rescaled_data_ = std::make_unique<uint8_t[]>(pow(kRescaleWidth, 2) *
-                                                 height_ / width_ * channels_);
+    rescaled_data_ = std::make_unique<uint8_t[]>(static_cast<size_t>(
+        pow(kRescaleWidth, 2) * height_ / width_ * channels_));
     stbir_resize_uint8(data_, width_, height_, 0, rescaled_data_.get(),
                        kRescaleWidth, kRescaleWidth * height_ / width_, 0,
                        channels_);
@@ -135,7 +148,7 @@ Color ImagePaletteGenerator::GetDominantColor() {
   if (pixels.empty()) {
     return Color{};
   }
-  auto dominant_count = 0;
+  size_t dominant_count = 0;
   auto dominant_color = Color{};
   auto color_map = std::unordered_map<Color, ColorCount>();
   for (auto i = 0; i < pixels.size(); i++) {
@@ -156,9 +169,48 @@ Color ImagePaletteGenerator::GetDominantColor() {
   return dominant_color;
 }
 
-std::vector<Color> ImagePaletteGenerator::GetRawPalette(
-    int32_t max_color_count) {
-  int32_t chunk_dimension = sqrt(max_color_count);
+std::vector<Color> ImagePaletteGenerator::GetPalette(int32_t max_color_count) {
+  max_color_count += 2;
+  auto pixels = GetPixels();
+  // No image loaded or pixels found. Return empty |tones_|.
+  if (pixels.empty()) {
+    return std::vector<Color>{};
+  }
+  if (!palette_.empty()) {
+    return palette_;
+  }
+  auto pixel_buffer = std::vector<uint8_t>{};
+  auto palette_buffer = std::make_unique<uint8_t[]>(max_color_count * 4);
+  // Clamp left & top to a minimum of 0.
+  auto left = 0 > left_bound_ ? 0 : left_bound_;
+  auto top = 0 > top_bound_ ? 0 : top_bound_;
+  // Clamp right & bottom to a maximum of the image dimensions.
+  auto right = right_bound_ > width_ ? width_ : right_bound_;
+  auto bottom = bottom_bound_ > height_ ? height_ : bottom_bound_;
+  for (auto i = left; i < right; i++) {
+    for (auto j = top; j < bottom; j++) {
+      pixel_buffer.emplace_back(pixels[i][j].r());
+      pixel_buffer.emplace_back(pixels[i][j].g());
+      pixel_buffer.emplace_back(pixels[i][j].b());
+      pixel_buffer.emplace_back(pixels[i][j].a());
+    }
+  }
+  exq_feed(static_cast<exq_data*>(exoquant_), pixel_buffer.data(),
+           static_cast<int32_t>(pixel_buffer.size() / 4));
+  exq_quantize_hq(static_cast<exq_data*>(exoquant_), max_color_count);
+  exq_get_palette(static_cast<exq_data*>(exoquant_), palette_buffer.get(),
+                  max_color_count);
+  for (auto i = 0; i < max_color_count; i++) {
+    auto color = Color{palette_buffer[i * 4 + 0], palette_buffer[i * 4 + 1],
+                       palette_buffer[i * 4 + 2], palette_buffer[i * 4 + 3]};
+    palette_.emplace_back(color);
+  }
+  exq_free(static_cast<exq_data*>(exoquant_));
+  return std::vector<Color>{palette_.begin() + 1, palette_.end() - 1};
+}
+
+std::vector<Color> ImagePaletteGenerator::GetTones(int32_t max_color_count) {
+  auto chunk_dimension = static_cast<int32_t>(sqrt(max_color_count));
   // Clamp left & top to a minimum of 0.
   auto left = 0 > left_bound_ ? 0 : left_bound_;
   auto top = 0 > top_bound_ ? 0 : top_bound_;
@@ -166,20 +218,16 @@ std::vector<Color> ImagePaletteGenerator::GetRawPalette(
   auto right = right_bound_ > width_ ? width_ : right_bound_;
   auto bottom = bottom_bound_ > height_ ? height_ : bottom_bound_;
   auto pixels = GetPixels();
-  // No image loaded or pixels found. Return empty palette_.
+  // No image loaded or pixels found. Return empty |tones_|.
   if (pixels.empty()) {
     return std::vector<Color>{};
   }
   // Already calculated.
-  if (!palette_.empty()) {
-    return palette_;
+  if (!tones_.empty()) {
+    return tones_;
   }
   for (auto i = left; i < right; i += ((right - left) / chunk_dimension)) {
     for (auto j = top; j < bottom; j += ((bottom - top) / chunk_dimension)) {
-      if (std::find(palette_.begin(), palette_.end(), pixels_[i][j]) ==
-          palette_.end()) {
-        palette_.emplace_back(pixels_[i][j]);
-      }
       // Browse through a chunk of image & calculate the averaged color.
       //
       // i ---------- i + ((|right| - |left|) / |chunk_dimension|)
@@ -189,25 +237,15 @@ std::vector<Color> ImagePaletteGenerator::GetRawPalette(
       //   ----------
       // j           j + ((|bottom - |top|) / |chunk_dimension|)
       auto averaged_pixel_color = GetAveragePixelAt(i, j, chunk_dimension);
-      if (std::find(normalized_palette_.begin(), normalized_palette_.end(),
-                    averaged_pixel_color) == normalized_palette_.end()) {
-        normalized_palette_.emplace_back(averaged_pixel_color);
+      if (std::find(tones_.begin(), tones_.end(), averaged_pixel_color) ==
+          tones_.end()) {
+        tones_.emplace_back(averaged_pixel_color);
       }
     }
   }
-  std::sort(palette_.begin(), palette_.end(),
+  std::sort(tones_.begin(), tones_.end(),
             [](Color a, Color b) { return a.Luminance() > b.Luminance(); });
-  std::sort(normalized_palette_.begin(), normalized_palette_.end(),
-            [](Color a, Color b) { return a.Luminance() > b.Luminance(); });
-  return palette_;
-}
-
-std::vector<Color> ImagePaletteGenerator::GetPalette(int32_t max_color_count) {
-  if (!normalized_palette_.empty()) {
-    return normalized_palette_;
-  }
-  GetRawPalette(max_color_count);
-  return normalized_palette_;
+  return tones_;
 }
 
 Color ImagePaletteGenerator::GetAveragePixelAt(int32_t x, int32_t y,
--- image_palette_generator.h
+++ image_palette_generator.h
@@ -1,5 +1,5 @@
-/// This file is a part of image_palette_generator
-/// (https://github.com/alexmercerind/image_palette_generator).
+/// This file is a part of image_tones_generator
+/// (https://github.com/alexmercerind/image_tones_generator).
 ///
 /// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
 /// All rights reserved.
@@ -9,25 +9,21 @@
 #ifndef IMAGE_PALETTE_GENERATOR_H_
 #define IMAGE_PALETTE_GENERATOR_H_
 
-#ifdef _WIN32
-#define DLLEXPORT __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
 #include <memory>
 #include <string>
 #include <vector>
 
 #include "color.h"
 
-class DLLEXPORT ImagePaletteGenerator {
+class ImagePaletteGenerator {
  public:
+  ImagePaletteGenerator();
+
   void Open(uint8_t* buffer, int32_t size);
 
   void Open(FILE* file);
 
-  void Open(std::string file_name);
+  void Open(char* file_name);
 
   void SetQuantized(bool quantized);
 
@@ -41,11 +37,10 @@ class DLLEXPORT ImagePaletteGenerator {
 
   Color GetDominantColor();
 
-  std::vector<Color> GetRawPalette(
-      int32_t max_color_count = kDefaultPaletteColorCount);
-
   std::vector<Color> GetPalette(
       int32_t max_color_count = kDefaultPaletteColorCount);
+
+  std::vector<Color> GetTones(int32_t max_color_count = kDefaultToneColorCount);
 
   ~ImagePaletteGenerator();
 
@@ -60,12 +55,13 @@ class DLLEXPORT ImagePaletteGenerator {
 
   Color GetQuantizedColor(Color color);
 
-  static constexpr auto kDefaultPaletteColorCount = 16;
+  static constexpr auto kDefaultPaletteColorCount = 8;
+  static constexpr auto kDefaultToneColorCount = 16;
   static constexpr auto kQuantizationWordWidth = 5;
   static constexpr auto kQuantizationChannelWidth = 8;
   static constexpr auto kRescaleWidth = 200;
 
-  bool quantized_ = false;
+  bool quantized_ = true;
   uint8_t* data_ = nullptr;
   std::unique_ptr<uint8_t[]> rescaled_data_ = nullptr;
   int32_t width_ = 0;
@@ -77,7 +73,8 @@ class DLLEXPORT ImagePaletteGenerator {
   int32_t bottom_bound_ = INT32_MAX;
   std::vector<std::vector<Color>> pixels_ = {};
   std::vector<Color> palette_ = {};
-  std::vector<Color> normalized_palette_ = {};
+  std::vector<Color> tones_ = {};
+  void* exoquant_ = nullptr;
 };
 
 #endif  // IMAGE_PALETTE_GENERATOR_H_
